3c723651dae10ee0e170acaeb4f5f616db17d8eb
tvndra11 <128711464+tvndra11@users.noreply.github.com>
2024-11-15 09:24:49 +0000

    made it work

--- burger.py
+++ burger.py
@@ -1,4 +1,12 @@
 import threading
+import random
 import functionsforburger
+foodlist = ['burger','fries','hotdog','cheeseburger','chicken','soda','slice of ham','pasta','smoothie','crisps']
 print('intro here')
-while 
+while money < 1000000:
+    food = foodlist[random.randint(0,9)]
+    print(food,'please')
+    answer = ""
+    while answer != food:
+        answer = input("")
+    print('order done')
