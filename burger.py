e2208d4907a9cea09b0fb7e3f892846aa1ce1f51
tvndra11 <128711464+tvndra11@users.noreply.github.com>
2024-11-15 09:39:33 +0000

    e

--- burger.py
+++ burger.py
@@ -3,10 +3,13 @@ import random
 import functionsforburger
 foodlist = ['burger','fries','hotdog','cheeseburger','chicken','soda','slice of ham','pasta','smoothie','crisps']
 print('intro here')
-while money < 1000000:
-    food = foodlist[random.randint(0,9)]
-    print(food,'please')
-    answer = ""
-    while answer != food:
-        answer = input("")
-    print('order done')
+
+while money < 1000000: 
+    while customers > 0:
+        
+        food = foodlist[random.randint(0,9)]
+        print(food,'please')
+        answer = ""
+        while answer != food:
+            answer = input("")
+        print('order done')
--- functionsforburger.py
+++ functionsforburger.py
@@ -1,1 +1,5 @@
-def start
+def customerline(wait):
+    import time
+    global customers 
+    customers = customers + 1
+    time.sleep(wait)
